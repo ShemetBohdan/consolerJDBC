2e127ec6f9c52c09daadcb0571a452fdd8d8dacb
Bohdan <shemet.bohdan01@gmail.com>
2022-07-07 18:52:53 +0300

    Initial commit

---
+++ src/main/java/com/example/consolerjdbc/ConsolerJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.consolerjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolerJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsolerJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consolerjdbc/ConsolerJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consolerjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolerJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
